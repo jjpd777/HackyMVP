f8e05ebff68cdc511f4f77fbcb61850260e836b8
juan <jpalacio@axletravel.com>
2020-10-09 07:52:41 -0600

    Pretty look for Chinito

--- src/menu.tsx
+++ src/menu.tsx
@@ -42,6 +42,16 @@ export const menuItemsMock = [
   {
     id: 5,
     category: 'Clásicos',
+    name: 'Arroz del Chinito',
+    description: 'Nuestro ya clásico arroz frito. Lleno de sabor, listo para que lo disfrutes. Pollo o cerdo, Q49 Res, Q51 Camarón, Q57 Mixto, Q64',
+    price: 49.0,
+    brief:'¡Inperdible!',
+    image:
+      'https://images.squarespace-cdn.com/content/v1/502eafb484ae7fae2e634f8b/1594682856325-VU0HUJLA75YB78OR9713/ke17ZwdGBToddI8pDm48kPJXHKy2-mnvrsdpGQjlhod7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmihaE5rlzFBImxTetd_yW5btdZx37rH5fuWDtePBPDaHF5LxdCVHkNEqSYPsUQCdT/Arroz-Frito.jpg',
+  },
+  {
+    id: 6,
+    category: 'Clásicos',
     name: 'Wantanes',
     description: 'Famosos por ser los más crujientes. Rellenos de res o camarón, acompañados de salsa agridulce. Escoge porciones de 6 y 12 unidades.',
     price: 32.0,
@@ -50,13 +60,13 @@ export const menuItemsMock = [
       'https://images.squarespace-cdn.com/content/v1/502eafb484ae7fae2e634f8b/1594682881743-FHM5458AWMIU0V79OETE/ke17ZwdGBToddI8pDm48kPJXHKy2-mnvrsdpGQjlhod7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmihaE5rlzFBImxTetd_yW5btdZx37rH5fuWDtePBPDaHF5LxdCVHkNEqSYPsUQCdT/Wan-Tan.jpg',
   },
   {
-    id: 6,
+    id: 7,
     category: 'Clásicos',
-    name: 'Arroz del Chinito',
-    description: 'Nuestro ya clásico arroz frito. Lleno de sabor, listo para que lo disfrutes. Pollo o cerdo, Q49 Res, Q51 Camarón, Q57 Mixto, Q64',
-    price: 49.0,
-    brief:'¡Inperdible!',
+    name: 'Wantanes',
+    description: 'Famosos por ser los más crujientes. Rellenos de res o camarón, acompañados de salsa agridulce. Escoge porciones de 6 y 12 unidades.',
+    price: 44.0,
+    brief:'Res Q44  unidades / Camarón Q49 12 unidades',
     image:
-      'https://images.squarespace-cdn.com/content/v1/502eafb484ae7fae2e634f8b/1594682856325-VU0HUJLA75YB78OR9713/ke17ZwdGBToddI8pDm48kPJXHKy2-mnvrsdpGQjlhod7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmihaE5rlzFBImxTetd_yW5btdZx37rH5fuWDtePBPDaHF5LxdCVHkNEqSYPsUQCdT/Arroz-Frito.jpg',
+      'https://images.squarespace-cdn.com/content/v1/502eafb484ae7fae2e634f8b/1594682881743-FHM5458AWMIU0V79OETE/ke17ZwdGBToddI8pDm48kPJXHKy2-mnvrsdpGQjlhod7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmihaE5rlzFBImxTetd_yW5btdZx37rH5fuWDtePBPDaHF5LxdCVHkNEqSYPsUQCdT/Wan-Tan.jpg',
   },
 ];
