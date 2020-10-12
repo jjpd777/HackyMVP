d32c2d2d745b13748578622319313f72eccdcc36
juan <jpalacio@axletravel.com>
2020-10-12 17:31:12 -0600

    fire baby

--- src/containers/Checkout/Checkout.tsx
+++ src/containers/Checkout/Checkout.tsx
@@ -105,8 +105,8 @@ function Checkout(props: CheckoutProps) {
     if(!checkName || !checkAddress || !thisphone) return
     const getPayment = payment ? 'efectivo' : 'tarjeta';
 
-    let baseURL = "https://wa.me/50256243902?text=";
-    let textBody="Hola El Chinito Veloz!%0AMi nombre es *" +String(checkName)+"* y me interesa hacer un pedido a *"+String(checkAddress)+"*." + ".%0A%0AMi pedido es el siguiente:%0A";
+    let baseURL = "https://wa.me/50242399603?text=";
+    let textBody="Hola Pollo Granjero!%0AMi nombre es *" +String(checkName)+"* y me interesa hacer un pedido a *"+String(checkAddress)+"*." + ".%0A%0AMi pedido es el siguiente:%0A";
     let finalpart = "*Total*%20Qtz.%20" +String(props.totalCartValue)+ "%0A%0AQuiero por favor pagar en *"+ getPayment+ "*. Muchas gracias de antemano%21"
 
     cart.forEach((cartItem) => {
